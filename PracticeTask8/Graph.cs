4b81dce70cd01ae91fbf3386daca65acad1c9c0c
KlichnikovDA <spacerus99@gmail.com>
2018-06-08 00:07:21 +0500

    Реализовал алгоритм выделения блоков в графе.

--- PracticeTask8/Graph.cs
+++ PracticeTask8/Graph.cs
@@ -15,10 +15,16 @@ namespace PracticeTask8
         int Columns;
         // Граф задается матрицей инциденций
         byte[,] IncidenceMatrix;
+        // Количество блоков
+        int Blocks;
         // Количество обследованных вершин
         int CheckedCount;
-        // Порядковые номера пройденных вершин
-        byte[] Positions;
+        // Порядковые номера обхода вершин
+        int[] Checked;
+        // Минимальные номера
+        int[] Minim;
+        // Стек с номерами ребер, составляющими блок
+        Stack<int> EdgesStack;
 
         // Конструктор
         public Graph(int Rows, int Columns, byte[,] Matrix)
@@ -26,10 +32,14 @@ namespace PracticeTask8
             this.Rows = Rows;
             this.Columns = Columns;
             IncidenceMatrix = Matrix;
+            Blocks = 0;
             CheckedCount = 0;
+            EdgesStack = new Stack<int>(Columns);
+            Checked = new int[Rows];
+            Minim = new int[Rows];
             for (int i = 0; i < Rows; i++)
             {
-                Positions[i] = 0;
+                Checked[i] = Minim[i] = 0;
             }
         }
 
@@ -47,7 +57,7 @@ namespace PracticeTask8
                 // Флаг правильности ввода
                 Size = sr.Read();
                 bool ok = Int32.TryParse(sr.ReadLine(), out Edges);
-               // Матрица инциденций
+                // Матрица инциденций
                 byte[,] Matrix = new byte[Size, Size];
                 for (int i = 0; i < Size; i++)
                 {
@@ -110,11 +120,64 @@ namespace PracticeTask8
             File.Close();
         }
 
-        // Поиск блоков посредством обхода в глубину
-        public void DepthSearch(int Pos, int Enter)
+        // Выделение блоков графа методом поиска в глубину
+        public void Block()
         {
-            CheckedCount++;
+            CheckedCount = 0;
+            // Проход по всем вершинам графа
+            for (int v = 0; v < Rows; v++)
+                // Если вершина еще не была пройдена
+                if (Checked[v] == 0)
+                    // Начинаем поиск из нее
+                    DFS(v, -1);
+        }
 
+        // Поиск в глубину с выделением ребер, составляющих блоки
+        void DFS(int Pos, int Parent)
+        {
+            // Отмечаем вершину
+            Checked[Pos] = Minim[Pos] = ++CheckedCount;
+
+            // Перебираем смежные вершины
+            for (int Edge = 0; Edge < Columns; Edge++)
+            {
+                // Смежная вершина
+                int NextVerit = Pos + 1;
+                // Перебираем ребра, исходящие из (или входящие в) исходной вершины
+                if (IncidenceMatrix[Pos, Edge] == 1)
+                {
+                    // Ищем вторую вершину данного ребра 
+                    while (NextVerit < Rows && IncidenceMatrix[NextVerit, Edge] == 0)
+                        NextVerit++;
+                    // Если нашли еще не пройденную вершину, смежную исходной, то
+                    if (NextVerit < Rows && Checked[NextVerit] == 0)
+                    {
+                        // Записываем номер ребра, соединяющего их
+                        EdgesStack.Push(NextVerit);
+                        // Продолжаем поиск из этой вершины
+                        DFS(NextVerit, Pos);
+                        // Переопределяем минимальный номер для вершины
+                        Minim[Pos] = Math.Min(Minim[NextVerit], Minim[Pos]);
+                        // Нашли корень дерева или точку сочленения
+                        if (Minim[NextVerit] > Minim[Pos])
+                        // Печатаем блок
+                        {
+                            Console.Write("Блок {0} состоит из ребер под номерами ", ++Blocks);
+                            while (EdgesStack.Count > 0)
+                                Console.Write("{0}, ", EdgesStack.Pop() + 1);
+                        }
+                    }
+                }
+                //else
+                //{
+                //    // Если вершина уже исследована и 
+                //    if (Checked[NextVerit] < Checked[Pos] && NextVerit != Parent)
+                //    {
+                //        EdgesStack.Push(Edge);
+                //        Minim[Pos] = Math.Min(Minim[NextVerit], Minim[Pos]);
+                //    }
+                //}
+            }                
         }
     }
 }
